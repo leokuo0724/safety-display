9a362c3e81ed277fb9da61023131dcb0548f2330
Leo Kuo <p900372012@gmail.com>
2020-11-06 23:58:56 +0800

    init commit

---
+++ src/views/Display.vue
@@ -0,0 +1,97 @@
+<template lang="pug">
+  .display-root
+    .display-text(ref="displayText") {{ displayText }}
+    router-link(class="back-btn" to="/")
+      .back-btn__img
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timer: null
+    };
+  },
+  computed: {
+    displayText() {
+      let text = window.localStorage.getItem("displayText");
+      if (!text || text === "") {
+        text = "慢";
+      }
+      return text;
+    }
+  },
+  mounted() {
+    const size = screen.width >= screen.height ? screen.height : screen.width;
+    this.$refs.displayText.style.fontSize = size + "px";
+
+    // exceed width, fire marquee effect
+    if (window.localStorage.getItem("displayText").length > 1) {
+      console.log("width: ", this.$refs.displayText.offsetWidth);
+      this.marqueeEffect();
+    }
+  },
+  beforeDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  },
+  methods: {
+    marqueeEffect() {
+      const text = this.$refs.displayText;
+      this.timer = setInterval(() => {
+        text.scrollLeft+=10;
+        if (text.scrollLeft >= text.scrollWidth - screen.width) {
+          text.scrollLeft = 0;
+        }
+      }, 30);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.display-root {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .display-text {
+    overflow: scroll;
+    white-space: nowrap;
+    animation: breath 0.7s;
+    animation-iteration-count: infinite;
+  }
+}
+@keyframes breath {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.back-btn {
+  position: fixed;
+  top: 24px;
+  right: 24px;
+  width: 48px;
+  height: 48px;
+  background: white;
+  border-radius: 12px;
+  box-shadow: 0px 5px 20px 5px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__img {
+    width: 50%;
+    height: 50%;
+    background-image: url("~@/assets/close-24px.svg");
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  .home-root
+    .form
+      h1 APP 名稱
+      .form__input-section
+        p 請輸入欲顯示之文字
+        input(
+          type="text"
+          v-model="textVal"
+          ref="inputText"
+          @focus="setFocusStyle(true)"
+          @blur="setFocusStyle(false)"
+        )
+        .deco(ref="deco")
+      //- router-link(class="form__button" to="/about") 確認
+      .form__button(@click="toDisplay") 確認
+    .warning-wrapper
+      .warning
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      textVal: "慢"
+    };
+  },
+  mounted() {
+    this.$refs.inputText.focus();
+  },
+  methods: {
+    toDisplay() {
+      // TODO: 太多字
+      // this.textVal.length 
+      window.localStorage.setItem("displayText", this.textVal);
+      this.$router.push("display");
+    },
+    setFocusStyle(bool) {
+      if (bool) {
+        this.$refs.deco.style.width = "100%";
+      } else {
+        this.$refs.deco.style.width = "0px";
+      }
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home-root {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form {
+    width: 300px;
+    height: 230px;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 8px;
+    box-shadow: 0px 5px 20px 5px rgba(0, 0, 0, 0.2);
+    &__input-section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      > p:nth-child(1) {
+        margin-bottom: 6px;
+        text-align: center;
+      }
+      > input:nth-child(2) {
+        height: 32px;
+        border: none;
+        padding: 0 8px;
+        background: rgba(#e5e5e5, 0.4);
+        // border-bottom: 2px solid #0047ff;
+      }
+      .deco {
+        width: 10%;
+        height: 3px;
+        background: #0047ff;
+        transition: width 0.2s;
+      }
+    }
+    &__button {
+      background: #0047ff;
+      padding: 6px 12px;
+      color: white;
+      border-radius: 6px;
+      box-shadow: 0px 5px 20px 3px rgba(#0047ff, 0.3);
+      text-decoration: none;
+    }
+  }
+}
+.warning-wrapper {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 54px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  .warning {
+    width: 96%;
+    height: 90%;
+    background: rgb(255, 71, 71);
+  }
+}
+</style>
